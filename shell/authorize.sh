b4d4b3ce5ba5cbe4f4f3ad6112143b81c71ebd0d
Phoenixwb <Phoenixwb@users.noreply.github.com>
2017-01-20 15:01:06 +0800

    Create authorize.sh

---
+++ shell/authorize.sh
@@ -0,0 +1,22 @@
+#bin/sh
+echo "check ssh key"
+cd ~
+if [ ! -d ".ssh/" ]; then
+echo "can not find .ssh"
+exit
+fi
+cd ~
+if [ ! -s ".ssh/id_rsa.pub" ]; then
+echo "creat ssh key"
+echo "\n"| ssh-keygen -t rsa
+fi
+key=$(cat ~/.ssh/id_rsa.pub)
+echo "ssh key is ready,authorizing..."
+echo "connect remote server..."
+echo "enter the server address:"
+read address
+echo "enter the server usr:"
+read usr
+echo "enter the server password:"
+ssh $usr@$address "mkdir ~/.ssh | echo "$key" >>~/.ssh/authorized_keys"
+echo "this device has been anthorized"
